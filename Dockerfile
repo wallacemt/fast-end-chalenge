effafff993f870024f15474587c0ee102198cbc4
Wallace Santana <wallacesantanak0@gmail.com>
2025-09-11 15:59:31 -0300

    build(ci): correct config

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM nginx:latest
-COPY ./dist /usr/share/nginx/html
+COPY . /usr/share/nginx/html
 EXPOSE 80
 CMD [ "nginx", "-g", "daemon off;" ]
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" { default = "us-east-1" }
-variable "key_name" { description = "SSH key pair name" }
-variable "public_key_path" { description = "Local public key to upload" }
+variable "key_name" { default = "deployer" }
+variable "public_key_path" { default = "~/terraform/deploy_key.pub" }
 variable "instance_count" { default = 3 }
 variable "instance_type" { default = "t3.micro" }
