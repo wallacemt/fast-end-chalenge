971f125ff76ae1d6bedc08c119a9f3f714751e75
root <root@Pczin.>
2025-09-11 21:04:23 -0300

    feat(./infra): create files for terraform and assible and tasing all

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" { default = "us-east-1" }
 variable "key_name" { default = "deployer" }
-variable "public_key_path" { default = "~/terraform/deploy_key.pub" }
+variable "public_key_path" { default = "../keys/deploy_key.pub" }
 variable "instance_count" { default = 3 }
 variable "instance_type" { default = "t3.micro" }
